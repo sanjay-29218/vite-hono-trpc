af58764dce0a0ea963a5766dafb8ef457e847a1c
sanjay-29218 <sanjay29218@gmail.com>
2025-10-01 17:29:47 +0545

    feat(server): add new getMessage2 endpoint to message router - Introduced `getMessage2` procedure in the message router returning a different greeting.

--- apps/server/src/trpc/routers/message.ts
+++ apps/server/src/trpc/routers/message.ts
@@ -2,6 +2,7 @@ import { publicProcedure, createTRPCRouter } from "../server.js";
 
 const messageRouter = createTRPCRouter({
   getMessage: publicProcedure.query(() => "hello tRPC v10!"),
+  getMessage2: publicProcedure.query(() => "hello tRPC v10!2"),
 });
 
 export default messageRouter;
