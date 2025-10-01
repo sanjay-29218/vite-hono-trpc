70b97aa925eb300b36668b68e680ef17902e7a22
sanjay-29218 <sanjay29218@gmail.com>
2025-10-01 17:48:19 +0545

    feat(server): add getMessage3 endpoint to message router - Introduced `getMessage3` procedure in the message router returning a new greeting.

--- apps/server/src/trpc/routers/message.ts
+++ apps/server/src/trpc/routers/message.ts
@@ -3,6 +3,7 @@ import { publicProcedure, createTRPCRouter } from "../server.js";
 const messageRouter = createTRPCRouter({
   getMessage: publicProcedure.query(() => "hello tRPC v10!"),
   getMessage2: publicProcedure.query(() => "hello tRPC v10!2"),
+  getMessage3: publicProcedure.query(() => "hello tRPC v10!3"),
 });
 
 export default messageRouter;
